f241dfad4b2b6d1038f5f44cd9fb186a7b27cc09
LOLFUTUREVEVO <144552882+LOLFUTUREVEVO@users.noreply.github.com>
2024-01-08 22:42:57 -0500

    lol

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Auto/ATOMQualOneAuto3.java
@@ -0,0 +1,68 @@
+package org.firstinspires.ftc.teamcode.OpModes.Auto;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Subsystems.ArmDepositor;
+import org.firstinspires.ftc.teamcode.Subsystems.Climber;
+import org.firstinspires.ftc.teamcode.Subsystems.Robot;
+
+@Config
+@Autonomous(name = "leftRed")
+public class ATOMQualOneAuto extends OpMode {
+
+
+    public Robot dt;
+    public Climber c;
+    public ArmDepositor arm;
+
+    @Override
+    public void init() {
+        dt = new Robot(hardwareMap);
+        c = new Climber(hardwareMap);
+        arm = new ArmDepositor(hardwareMap);
+
+    }
+
+
+    boolean s1=false,s2=false;
+
+
+    @Override
+    public void start() {
+        dt.moveInches(-0.36, -0.36, -0.36, -0.36);
+        dt.moveInches(-1.6, 1.6, -1.6, 1.6);
+        dt.moveInches(-5, -5, -5, -5);
+        dt.moveInches(-1,1,1,-1);
+        dt.moveInches(-2.5,-2.5,-2.5,-2.5);
+    }
+
+    @Override
+    public void loop() {
+
+
+        ElapsedTime PidTimer = new ElapsedTime();
+
+
+
+
+
+
+
+
+    }
+
+    @Override
+    public void stop() {
+        telemetry.addData("OpMode Stopping","Auto Ending");
+        telemetry.addData("Readying","TeleOp");
+        telemetry.update();
+    }
+
+
+
+
+}
